7f26102fb00f2d4609af747f30d99c098397f541
matheanswer <36707250+matheanswer@users.noreply.github.com>
2020-11-05 23:45:21 +0100

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,7 +1,5 @@
-#include QMK_KEYBOARD_H
-
 /*
-Physical layout
+Physical layout on my keyboard
 ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
 │00 │01 │02 │03 │04 │05 │06 │07 │08 │09 │0a │0b │0c │0d │0e │
 ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
@@ -30,99 +28,36 @@ QMK standard arrangement "LAYOUT"
 31, 44 and 48 aren't used so leave blank with keycode XXXXXXX (or KC_NO)
 */
 
+#include QMK_KEYBOARD_H
+
 #define _BL 0
 #define _FL 1
 #define _CL 2
 
-
-enum custom_keycodes {
-  VOL_UP = SAFE_RANGE,
-  VOL_DN,
-  EMAIL
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case VOL_UP:
-      if (record->event.pressed) {
-        // when keycode VOL_UP is pressed
-        tap_code(KC_VOLU);
-        tap_code(KC_VOLU);
-        tap_code(KC_VOLU);
-        tap_code(KC_VOLU);
-        tap_code(KC_VOLU);
-      } else {
-        // when keycode VOL_UP is released
-      }
-      break;
-    case VOL_DN:
-      if (record->event.pressed) {
-        // when keycode VOL_DN is pressed
-        tap_code(KC_VOLD);
-        tap_code(KC_VOLD);
-        tap_code(KC_VOLD);
-        tap_code(KC_VOLD);
-        tap_code(KC_VOLD);
-      } else {
-        // when keycode VOL_DN is released
-      }
-      break;
-    case EMAIL:
-      if (record->event.pressed) {
-           // when keycode EMAIL is pressed
-                SEND_STRING("mathias.heyraud@gmail.com");
-      } else {
-        // when keycode EMAIL is released
-      }
-      break;
-  }
-  return true;
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	// Keymap _BL: Base Layer (Default Layer)
+     // Keymap _BL: Base Layer (Default)
      [_BL] = LAYOUT(
-		KC_GESC,  KC_1,     KC_2,     KC_3,	KC_4,	KC_5,	KC_6,	KC_7,	KC_8,	KC_9,	KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,
-		KC_TAB,   KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	KC_Y,	KC_U,	KC_I,	KC_O,	KC_P,	KC_LBRC,	KC_RBRC,	KC_BSPC,
-		KC_LCTL,	KC_A,	KC_S,	KC_D,	KC_F,	KC_G,	KC_H,	KC_J,	KC_K,	KC_L,	KC_SCLN,	KC_QUOT,	KC_ENT,
-		KC_LSFT,	XXXXXXX,	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,	KC_N,	KC_M,	KC_COMM,	KC_DOT,	KC_SLSH,	KC_RSFT,	KC_DEL,
-		MO(_FL),	KC_LGUI,	KC_RALT,	XXXXXXX,	KC_SPC,	XXXXXXX,	KC_LALT,	KC_LEFT,	KC_DOWN,	KC_UP,	KC_RGHT),
+          KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,
+          KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
+          KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+          KC_LSFT,  XXXXXXX,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_DEL,
+          MO(_FL),  KC_LGUI,  KC_LALT,  XXXXXXX,  KC_SPC,   XXXXXXX,  KC_RALT,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT   ),
 
      // Keymap _FL: Function Layer
-	[_FL] = LAYOUT(
-		TO(_CL),  KC_F1,    KC_F2,    KC_F3,	KC_F4,	KC_F5,	KC_F6,	KC_F7,	KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  _______,  _______,  _______,
-		KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-	     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  VOL_DN,   VOL_UP,   _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END),
+     [_FL] = LAYOUT(
+          TO(_CL),  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,
+          C(KC_TAB),_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  _______,  _______,  KC_DEL,
+          KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,
+          _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END    ),
 
      // Keymap _CL: Control Layer
-	[_CL] = LAYOUT(
-		TG(_CL),  RGB_MOD,  RGB_RMOD, RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,
-		_______,  _______,  _______,  EEP_RST,  RESET,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______),
-		
+     [_CL] = LAYOUT(
+          TG(_CL),  RGB_MOD,  RGB_RMOD, RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,
+          _______,  _______,  _______,  EEP_RST,  RESET,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______   ),
+
 };
-
-
-// // Underglow color depending on layer
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//      switch (get_highest_layer(state)) {
-
-//      case _CL:
-//           rgblight_enable_noeeprom();
-//           rgblight_setrgb (RGB_GREEN);
-//           break;
-//      case _FL:
-//           rgblight_enable_noeeprom();
-//           rgblight_setrgb (RGB_BLUE);
-//           break;
-//      default: //  for any other layers, or the default layer
-//           rgblight_disable_noeeprom();
-//           break;
-//      }
-//      return state;
-// }
